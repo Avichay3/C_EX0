0c0a55e422309f9a9ea2298c7c618ea77246dab2
Avichay3 <aviaimzina123456@gmail.com>
2022-11-22 14:12:02 +0200

    add a isPalindrome with recursion

--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -23,6 +23,20 @@ int isArmstrong(int y){
     }
 }
 
-int isPalindrome(int){
+
+  int reverse (int num){ //get the reverse number
+    int digit = log10(num);
+    if(num == 0){
+        return 0;
+    }
+    return (int)((num % 10 * pow(10, digit))+reverse (num / 10));
+}
+
+   int isPalindrome(int num){ //if it is palindrome return 1, else return 0
+    if (num == reverse(num)){
+        return 1;
+    }
     return 0;
 }
+    
+ 
